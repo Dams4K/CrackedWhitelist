4881c66a9310d034bf922d383718a425ba290dce
Dams4K <dams4k@users.noreply.github.com>
2025-07-01 21:30:34 +0200

    doc

--- src/main/java/fr/dams4k/crackedwhitelist/mixin/MixinUserWhiteList.java
+++ src/main/java/fr/dams4k/crackedwhitelist/mixin/MixinUserWhiteList.java
@@ -20,14 +20,18 @@ public abstract class MixinUserWhiteList extends StoredUserList<GameProfile, Use
         super(p_11380_);
     }
 
+    /**
+     * @author Dams4K
+     * @reason A new file is used to store whitelisted players to not override the existing whitelist.json file
+     */
     @ModifyVariable(method = "<init>", at = @At("LOAD"))
     private static File injectFile(File file) {
         return new File(file.getParentFile(), "offline_" + file.getName());
     }
 
     /**
-     * @author
-     * @reason
+     * @author Dams4K
+     * @reason getKeyForUser is used to get the uuid of the profile. Leading me to returning the offline uuid
      */
     @Overwrite
     protected String getKeyForUser(GameProfile gameProfile) {
